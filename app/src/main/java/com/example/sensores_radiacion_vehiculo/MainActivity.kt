497f814c44b8ab2648117314b0f9fcb949f47b68
Marina <marina.alvarezh@alumnos.upm.es>
2023-04-18 23:17:39 +0200

    Pantalla inicial con imagen de icono Bluetooth y cabecera

--- app/src/main/java/com/example/sensores_radiacion_vehiculo/MainActivity.kt
+++ app/src/main/java/com/example/sensores_radiacion_vehiculo/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.sensores_radiacion_vehiculo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.sensores_radiacion_vehiculo.ui.theme.Sensores_radiacion_vehiculoTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +27,9 @@ class MainActivity : ComponentActivity() {
             Sensores_radiacion_vehiculoTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
-                    //prueba developer
-                    //prueba screen
+                    ScreenOne()
                 }
             }
         }
@@ -32,14 +37,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ScreenOne(){
+    HeaderScreenOne(name = stringResource(R.string.Beacon_Simulator_Text))
+    Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceAround, horizontalAlignment = Alignment.CenterHorizontally) {
+        ImageBluetoothScreenOne()
+    }
+}
+
+@Composable
+fun HeaderScreenOne(name: String) {
+    Surface(color= Color.Blue) {
+        Text(
+            text = "$name",
+            color = Color.White,
+            fontSize = 27.sp,
+            modifier = Modifier
+                .padding(12.dp)
+                .fillMaxWidth(),
+            textAlign = TextAlign.Start
+        )
+    }
+}
+
+@Composable
+fun ImageBluetoothScreenOne(){
+    val image = painterResource(R.drawable.bluetooth_icon)
+    Image(
+        painter = image,
+        contentDescription = null,
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Sensores_radiacion_vehiculoTheme {
-        Greeting("Android")
+        ScreenOne()
     }
 }
